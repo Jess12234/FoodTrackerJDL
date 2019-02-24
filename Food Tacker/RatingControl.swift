43510d4f747892b797d881936497ee29595da33b
Jessica Lowry <jdl14a@lib-301-06-9114.local>
2019-02-24 17:01:16 -0600

    Added Rating Control

---
+++ Food Tacker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  Food Tacker
+//
+//  Created by Jessica Lowry on 2/24/19.
+//  Copyright © 2019 Jessica Lowry. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
